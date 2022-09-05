d87115e8a67e970c0f8737bee680130981361c45
Jesper Christ <jesper@jfc-lap.localdomain>
2022-09-05 20:13:29 +0200

    added image blog and image category

---
+++ src/api/image-category/controllers/image-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  image-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::image-category.image-category');
---
+++ src/api/image-category/routes/image-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * image-category router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::image-category.image-category');
